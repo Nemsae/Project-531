6124143f39a03f76c66616a597de811e9023abfa
Zhaggym <zhaggm@gmail.com>
2016-11-22 08:57:43 +0100

    added inputpage to routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,12 +3,14 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
+import InputPage from './components/Halfer/InputPage';
 // import Dashboard from './components/ACL/Dashboard';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
     <Route path="about" component={AboutPage} />
+    <Route path="inputpage" component={InputPage} />
     {/* <Route path="dashboard" component={Dashboard} /> */}
   </Route>
   );
